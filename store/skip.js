38202f310086322d63c577be672338e6c152ea09
HannesOberreiter <hoberreiter@gmail.com>
2020-08-24 21:18:53 +0200

    :newspaper: allow 10 news per page

--- store/skip.js
+++ store/skip.js
@@ -1,20 +1,21 @@
 export const state = () => ({
   value: 0,
   max: 0,
+  jump: 10,
   more: true
 })
 
 export const mutations = {
   next (state) {
-    state.value = state.value + 4;
-    state.more = state.value + 4 < state.max ? true : false;
+    state.value = state.value + state.jump;
+    state.more = state.value + state.jump < state.max ? true : false;
   },
   start (state) {
     state.value = 0;
     state.more = state.value < state.max ? true : false;
   },
   previous (state) {
-    state.value = state.value == 0 ? 0 : state.value - 4;
+    state.value = state.value == 0 ? 0 : state.value - state.jump;
     state.more = state.value < state.max ? true : false;
   },
   set_limit (state, limit) {
