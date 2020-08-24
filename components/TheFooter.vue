e49f8dff369873384e7825e152b561da28d36350
HannesOberreiter <hoberreiter@gmail.com>
2020-08-24 20:09:56 +0200

    changed footer copyright notice

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -2,7 +2,7 @@
   <footer class="h-20 w-full border-t dark:border-gray-800 bg-white dark:bg-gray-900">
     <div class="h-full w-full container mx-auto px-4 lg:px-8 flex items-center justify-between">
       <div class="flex items-end">
-        <span class="mr-2 text-sm leading-none">© {{ new Date().getFullYear() }} - b.tree by Hannes Oberreiter</span>
+        <span class="mr-2 text-sm leading-none">© {{ new Date().getFullYear() }} - Project Monogeniac</span>
       </div>
       <Socialicons socialclass='flex' />
     </div>
